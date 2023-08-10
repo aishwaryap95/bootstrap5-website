e5a3df194e57145774436ae5bdf8775c00bf737b
Aishwarya Amar Patankar <106923039+aishwaryap95@users.noreply.github.com>
2023-08-10 20:09:46 +0530

    Video 45 completed

--- inotebook/backend/db.js
+++ inotebook/backend/db.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 function connectToMongo() {
-    mongoose.connect('mongodb://localhost:27017/mydb', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
+  mongoose
+    .connect("mongodb://localhost:27017/", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
     .then(() => {
-        console.log('Connected to MongoDB');
+      console.log("Connected to MongoDB");
     })
-    .catch(error => {
-        console.error('Error connecting to MongoDB:', error);
+    .catch((error) => {
+      console.error("Error connecting to MongoDB:", error);
     });
 }
 
