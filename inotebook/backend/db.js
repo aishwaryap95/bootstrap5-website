ff8c11232892bf71c36c674cfc75754a066db0ce
Aishwarya Amar Patankar <106923039+aishwaryap95@users.noreply.github.com>
2023-08-10 15:51:11 +0530

    Video 43 completed

---
+++ inotebook/backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+function connectToMongo() {
+    mongoose.connect('mongodb://localhost:27017/mydb', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(error => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+}
+
+module.exports = connectToMongo;
