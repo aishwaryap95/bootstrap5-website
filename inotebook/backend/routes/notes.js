f39838e019520b085a0c51256ddc2572784fada1
Aishwarya Amar Patankar <106923039+aishwaryap95@users.noreply.github.com>
2023-08-13 20:52:04 +0530

    52 add and fetch notes endpoints created

--- inotebook/backend/routes/notes.js
+++ inotebook/backend/routes/notes.js
@@ -1,8 +1,56 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator"); // Import express-validator
 const router = express.Router();
+var fetchuser = require("../middleware/fetchuser");
+const Note = require("../models/Note");
 
-router.get("/", (req, res) => {
-  
-  res.json([ ]);
+// Route 1 : Get all the notes using: Get "/api/auth/createuser".   Login required
+
+router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server Error");
+  }
 });
+
+// Route 2 : Add a new Note using: Post "/api/auth/addnote".   Login required
+
+router.post(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter valid title please").isLength({ min: 3 }),
+    body("description", "Description must be atleast 5 characters").isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+
+      // If there are errors, return bad request and the errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      // now create new note
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+      const saveNote = await note.save();
+      res.json(saveNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server Error");
+    }
+  }
+);
+
 module.exports = router;
