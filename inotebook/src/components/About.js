265e1aaa38a2134fcfa42438b1e1eb9509c62b64
Aishwarya Amar Patankar <106923039+aishwaryap95@users.noreply.github.com>
2023-08-16 20:35:20 +0530

    useLocation Recat hook used 59

--- inotebook/src/components/About.js
+++ inotebook/src/components/About.js
@@ -1,20 +1,7 @@
 import React, { useContext, useEffect } from "react";
 
-import noteContext from "../context/noteContext";
-
 const About = () => {
-  const a = useContext(noteContext);
-
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, []);
-
-  return (
-    <div>
-      this is about {a.state.name} and she is in class {a.state.class}
-    </div>
-  );
+  return <div>this is about Page</div>;
 };
 
 export default About;
--- inotebook/src/context/NoteState.js
+++ inotebook/src/context/NoteState.js
@@ -2,23 +2,8 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const s1 = {
-    name: "Aishwarya",
-    class: "4A",
-  };
-  const [state, setState] = useState(s1);
-  const update = () => {
-    setTimeout(() => {
-      setState({
-        name: "Larry",
-        class: "10d",
-      });
-    },1000);
-  };
   return (
-    <NoteContext.Provider value={{state, update}}>
-      {props.children}
-    </NoteContext.Provider>
+    <NoteContext.Provider value={[]}>{props.children}</NoteContext.Provider>
   );
 };
 
